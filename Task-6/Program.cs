bd43e9c117c6e44c54a2c956a40bfccab3bb1507
ladynazgool <ladynazgool@gmail.com>
2023-05-17 16:55:52 +0400

    Task 6 added

---
+++ Task-6/Program.cs
@@ -0,0 +1,34 @@
+﻿// Обход директории
+
+// string path = "/Games/minetest-5.4.1-win64/minetest-5.4.1-win64/textures/base/pack";
+// DirectoryInfo di = new DirectoryInfo(path);
+// System.Console.WriteLine(di.CreationTime);
+
+// FileInfo[] fi = di.GetFiles();
+
+// for (int i = 0; i < fi.Length; i++)
+// {
+//     System.Console.WriteLine(fi[i].Name);
+// } // это было написано предварительно, для ознакомления с классами DirectoryInfo FileInfo, сам код ниже!
+
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+
+string path = @"/Games/minetest-5.4.1-win64/minetest-5.4.1-win64";
+CatalogInfo(path);
