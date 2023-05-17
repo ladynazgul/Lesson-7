73af0c87467eef833435dc04cd643f97a68a7ff6
ladynazgool <ladynazgool@gmail.com>
2023-05-17 15:29:01 +0400

    Task 5 added

---
+++ Task-5/Program.cs
@@ -0,0 +1,38 @@
+﻿// Перебор слов
+
+// char[] s = { 'а', 'и', 'с', 'в' }; // итеративное решение
+// int count = s.Length;
+// int n = 1;
+
+// for (int i = 0; i < count; i++)
+// {
+//     for (int j = 0; j < count; j++)
+//     {
+//         for (int k = 0; k < count; k++)
+//         {
+//             for (int l = 0; l < count; l++)
+//             {
+//                 for (int m = 0; m < count; m++)
+//                 {
+//                     Console.WriteLine($"{n++, -5}{s[i]}{s[j]}{s[k]}{s[l]}{s[m]}");
+//                 }
+//             }
+//         }
+//     }
+// }
+
+int n = 1; // рекурсивный метод
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for (int i =0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+
+FindWords("аисв", new char[2]);
